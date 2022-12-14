000c9e7629ae43066dcb9dcba70605e6a3568222
Atman <41929192+Grawor@users.noreply.github.com>
2022-12-14 22:54:18 +0900

    Commit

---
+++ ContactManager.WPF/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ContactManager.WPF.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace ContactManager.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ ContactManagerTest.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ContactManagerTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
